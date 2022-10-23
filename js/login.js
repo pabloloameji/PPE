c482cedd5f589355206f2121bca02064d65fc6ed
Pablo Loaiza Mejía <pablo@loaizas.com>
2022-10-23 17:44:22 -0500

    login funcional

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 let btn_enviar, btn_cerrar_modal, formulario, ventana;
-let correo,clave, usuario, imagen, titulo,mensaje;
+let correo, clave, usuario, imagen, titulo, mensaje;
 
-function iniciarLogin(){
+function iniciarLogin() {
 	btn_enviar = document.getElementById("btn_enviar");
 	btn_cerrar_modal = document.getElementById("btn_cerrar_modal");
 	mensaje = document.querySelector("#ventana p");
@@ -11,44 +11,44 @@ function iniciarLogin(){
 	clave = document.getElementById("clave");
 	ventana = document.querySelector("#ventana");
 	formulario = document.getElementById("formulario");
-	formulario.addEventListener("submit",procesarLogin);
-	btn_cerrar_modal.addEventListener("click",cerrarVentana);
+	formulario.addEventListener("submit", procesarLogin);
+	btn_cerrar_modal.addEventListener("click", cerrarVentana);
 }
 
-function mensaje_exito(texto_mensaje){
-    imagen.innerHTML = "🥳"; 
-    btn_cerrar_modal.innerHTML = "Continuar";
-    titulo.innerHTML = "Yayy!";
-    mensaje.innerHTML = texto_mensaje;
-    titulo.classList.remove("color_error_texto");
-    titulo.classList.add("color_exito_texto");
-    btn_cerrar_modal.classList.remove("color_error_texto","color_error_borde")
-    btn_cerrar_modal.classList.add("color_exito_texto","color_exito_borde");
+function mensaje_exito(texto_mensaje) {
+	imagen.innerHTML = "🥳";
+	btn_cerrar_modal.innerHTML = "Continuar";
+	titulo.innerHTML = "Yayy!";
+	mensaje.innerHTML = texto_mensaje;
+	titulo.classList.remove("color_error_texto");
+	titulo.classList.add("color_exito_texto");
+	btn_cerrar_modal.classList.remove("color_error_texto", "color_error_borde")
+	btn_cerrar_modal.classList.add("color_exito_texto", "color_exito_borde");
 }
 
-function mensaje_error(texto_mensaje){
-	imagen.innerHTML = "😥"; 
-    btn_cerrar_modal.innerHTML = "Intenta nuevamente";
-    titulo.innerHTML = "Oops!";
-    mensaje.innerHTML = texto_mensaje;
-    titulo.classList.add("color_exito_texto");
-    titulo.classList.add("color_error_texto");
-    btn_cerrar_modal.classList.remove("color_exito_texto","color_exito_borde");
-    btn_cerrar_modal.classList.add("color_error_texto","color_error_borde");
+function mensaje_error(texto_mensaje) {
+	imagen.innerHTML = "😥";
+	btn_cerrar_modal.innerHTML = "Intenta nuevamente";
+	titulo.innerHTML = "Oops!";
+	mensaje.innerHTML = texto_mensaje;
+	titulo.classList.add("color_exito_texto");
+	titulo.classList.add("color_error_texto");
+	btn_cerrar_modal.classList.remove("color_exito_texto", "color_exito_borde");
+	btn_cerrar_modal.classList.add("color_error_texto", "color_error_borde");
 }
 
-function abrirVentana(){
+function abrirVentana() {
 	ventana.classList.remove("hidden");
 }
 
-function cerrarVentana(evento){
+function cerrarVentana(evento) {
 	//ventana.classList.add("hidden");
 	location.href = "index.html";
 }
 
-function procesarLogin(evento){
+function procesarLogin(evento) {
 
-	let txt_correo,txt_clave;
+	let txt_correo, txt_clave;
 	let str_usuario, md5_clave;
 	let error = false;
 	txt_correo = correo.value;
@@ -56,20 +56,20 @@ function procesarLogin(evento){
 
 	str_usuario = localStorage.getItem("usuario");
 	usuario = JSON.parse(str_usuario);
-	
-	if(usuario){
-		if(usuario.correo === txt_correo && usuario.clave === txt_clave){
+
+	if (usuario) {
+		if (usuario.correo === txt_correo && usuario.clave === txt_clave) {
 			cambiarSesion(true);
 			mensaje_exito("Muy bien, logueo exitoso, continua disfrutando del sitio.");
 		}
-		else{
+		else {
 			error = true;
 		}
-	}else{
+	} else {
 		error = true;
 	}
 
-	if(error){
+	if (error) {
 		mensaje_error("Revisa los datos, ocurrio un error.");
 	}
 
