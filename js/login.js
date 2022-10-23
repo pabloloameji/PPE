13ccce9e9d62eb75353e09f01d41dba870f1431c
Pablo Loaiza Mejía <pablo@loaizas.com>
2022-10-23 17:54:36 -0500

    corrección login

--- js/login.js
+++ js/login.js
@@ -3,48 +3,12 @@ let correo, clave, usuario, imagen, titulo, mensaje;
 
 function iniciarLogin() {
 	btn_enviar = document.getElementById("btn_enviar");
-	btn_cerrar_modal = document.getElementById("btn_cerrar_modal");
-	mensaje = document.querySelector("#ventana p");
-	titulo = document.querySelector("#ventana h1");
-	imagen = document.querySelector("#ventana span");
 	correo = document.getElementById("correo");
 	clave = document.getElementById("clave");
-	ventana = document.querySelector("#ventana");
 	formulario = document.getElementById("formulario");
 	formulario.addEventListener("submit", procesarLogin);
-	btn_cerrar_modal.addEventListener("click", cerrarVentana);
 }
 
-function mensaje_exito(texto_mensaje) {
-	imagen.innerHTML = "🥳";
-	btn_cerrar_modal.innerHTML = "Continuar";
-	titulo.innerHTML = "Yayy!";
-	mensaje.innerHTML = texto_mensaje;
-	titulo.classList.remove("color_error_texto");
-	titulo.classList.add("color_exito_texto");
-	btn_cerrar_modal.classList.remove("color_error_texto", "color_error_borde")
-	btn_cerrar_modal.classList.add("color_exito_texto", "color_exito_borde");
-}
-
-function mensaje_error(texto_mensaje) {
-	imagen.innerHTML = "😥";
-	btn_cerrar_modal.innerHTML = "Intenta nuevamente";
-	titulo.innerHTML = "Oops!";
-	mensaje.innerHTML = texto_mensaje;
-	titulo.classList.add("color_exito_texto");
-	titulo.classList.add("color_error_texto");
-	btn_cerrar_modal.classList.remove("color_exito_texto", "color_exito_borde");
-	btn_cerrar_modal.classList.add("color_error_texto", "color_error_borde");
-}
-
-function abrirVentana() {
-	ventana.classList.remove("hidden");
-}
-
-function cerrarVentana(evento) {
-	//ventana.classList.add("hidden");
-	location.href = "index.html";
-}
 
 function procesarLogin(evento) {
 
@@ -60,7 +24,8 @@ function procesarLogin(evento) {
 	if (usuario) {
 		if (usuario.correo === txt_correo && usuario.clave === txt_clave) {
 			cambiarSesion(true);
-			mensaje_exito("Muy bien, logueo exitoso, continua disfrutando del sitio.");
+			location.href = "index.html";
+			alert("logueo exitoso");
 		}
 		else {
 			error = true;
@@ -70,13 +35,8 @@ function procesarLogin(evento) {
 	}
 
 	if (error) {
-		mensaje_error("Revisa los datos, ocurrio un error.");
+		alert("Revisa los datos, ocurrio un error.");
 	}
 
-	abrirVentana();
 	evento.preventDefault();
-
-
-	//console.log(evento.target);
-
 }
